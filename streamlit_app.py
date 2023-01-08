a84a1af1a0d8b5ebe604531a05f2097a90867738
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-07 19:23:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,7 @@ ax2.xaxis.set_major_locator(MinuteLocator (interval=30))
 #ax2.legend(fontsize=6)
 # Add a horizontal line at y=30 for Oversold
 ax2.axhline(y=30, color='g')
-ax2.text(x=0, y=30, s='Oversold', horizontalalignment='left', verticalalignment='bottom', fontsize=6)
+ax2.text(x=0, y=30, s='Threshold', horizontalalignment='left', verticalalignment='bottom', fontsize=6)
 
 
 # Add a horizontal line at y=70 for Overbought
