9e26a19c7b6fd6a85274f227d4141bab17547e38
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-07 20:34:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -207,18 +207,18 @@ macd_df = calc_macd(data, 'Close', 12, 26, 9)
 # Plot the MACD and MACD histogram values
 ax3.plot(macd_df['MACD'], label='MACD', color='green')
 ax3.plot(macd_df['MACD signal'], label='MACD signal', color='red')
-ax3.bar(macd_df.index, macd_df['MACD histogram'], label='MACD histogram', width=0.01, color='purple', edgecolor='black', alpha=.5)
+#ax3.bar(macd_df.index, macd_df['MACD histogram'], label='MACD histogram', width=0.01, color='purple', edgecolor='black', alpha=.5)
 
-# # # Plot the bar chart
-# bars = ax3.bar(range(len(macd_df)), macd_df['MACD histogram'], label='MACD histogram', width=0.01, edgecolor='black', alpha=.5)
-# threshold = .001
+# # Plot the bar chart
+bars = ax3.bar(range(len(macd_df)), macd_df['MACD histogram'], label='MACD histogram', width=0.01, edgecolor='black', alpha=.5)
+threshold = .001
 
-# # Set the color of each bar based on the threshold value
-# for bar in bars:
-#     if bar.get_height() > threshold:
-#         bar.set_color('green')
-#     else:
-#         bar.set_color('red')
+# Set the color of each bar based on the threshold value
+for bar in bars:
+    if bar.get_height() > threshold:
+        bar.set_color('green')
+    else:
+        bar.set_color('red')
 
 ax3.set_xlabel('Time')
 ax3.set_ylabel('MACD')
