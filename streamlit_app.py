537a1388cf0705d18061401cc7a72ce3a9d9de08
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-14 22:07:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ option = st.sidebar.radio("Select Option Type: ", ('Call', 'Put'))
 
 strike = st.sidebar.slider("Select the option strike", 300,450,396)
 
-option_date = st.sidebar.date_input("Enter the strike date", datetime.date(2023,1,17))
+option_date = st.sidebar.date_input("Enter the strike date")
 
 st.write('The option is ' + symbol + option_date + option + '00' + strike + '000')
 
