214a9746411dcdf4ec04071faac6c67f346eb6dc
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-14 23:10:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ st.write(d)
 st.write(option_type)
 st.write(strike)
 
-options = ticker_label+d+option_type+'00'+strike+'000'
+options = ticker_label+d+option_type+'00'+str(strike)+'000'
 st.write('The option is ' + options)
 
 
