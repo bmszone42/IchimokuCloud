94e719e9dbcf2b94a99ec69f38ec864cbab1d76e
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-21 15:45:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -358,7 +358,7 @@ if result:
         file_name = options + ".png"
         file_path = st.file_uploader("Choose a location to save the file", type="png")
         if file_path:
-            if os.path.exists(file_path)
+            if os.path.exists(file_path):
             #file_name = st.text_input("Enter file name:", file_name)
             plt.savefig(file_path + '/' + file_name)
             st.success("Data saved successfully!")
