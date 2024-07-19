775bfa3d0f7984176bbd5abdd4b27e4839ab6a18
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2024-07-19 19:22:30 -0400

    Update streamlit_app.py (#9)
    updated range for market conditions

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ else:
     option_type = "P"
 
 # Add a slider input for the strike price
-strike = st.sidebar.slider("Select the option strike", 550, 590, 561)
+strike = st.sidebar.slider("Select the option strike", 500, 550, 581)
 
 # Add a input for the option date
 option_date = st.sidebar.date_input("Enter the strike date")
